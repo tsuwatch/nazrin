4a0c5813da2ced02874ccf6dcd2d2bc2e0874299
ru_shalm <ru_shalm@hazimu.com>
2016-06-16 20:24:35 +0900

    Do not override Kaminari.config.max_pages

--- lib/nazrin/pagination_generator/kaminari_generator.rb
+++ lib/nazrin/pagination_generator/kaminari_generator.rb
@@ -7,10 +7,11 @@ module Nazrin
         rescue LoadError
           abort "Missing dependency 'kaminari' for pagination"
         end
-        Kaminari.config.max_pages = options[:last_page]
         Kaminari.paginate_array(collection, total_count: options[:total_count])
           .page(options[:current_page])
-          .per(options[:per_page])
+          .per(options[:per_page]).tap do |paginate_array|
+            paginate_array.max_pages_per(options[:last_page])
+          end
       end
     end
   end
