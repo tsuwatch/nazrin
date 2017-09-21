16aec66a716b1044d1a57681aeaa36c76d8013f6
Yefim Vedernikoff <yefim323@gmail.com>
2017-09-21 15:07:04 -0700

    Allow options to be passed to model

--- lib/nazrin/data_accessor/mongoid.rb
+++ lib/nazrin/data_accessor/mongoid.rb
@@ -3,6 +3,13 @@ module Nazrin
     class Mongoid < Nazrin::DataAccessor
       def load_all(ids)
         documents_table = {}
+        @options.each do |k, v|
+          @model = if v.nil?
+                     @model.send(k)
+                   else
+                     @model.send(k, v)
+                   end
+        end
         @model.where('_id' => { '$in' => ids }).each do |document|
           documents_table[document._id.to_s] = document
         end
