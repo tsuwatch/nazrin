33fb76bca68cf288bf7084b3c46612424b76fd4c
AMHOL <andyholland1991@aol.com>
2019-03-11 14:51:22 +0800

    Ensure @model is immutable in Nazrin::DataAccessor's

--- lib/nazrin/data_accessor/active_record.rb
+++ lib/nazrin/data_accessor/active_record.rb
@@ -4,12 +4,14 @@ module Nazrin
       # load from activerecord
       def load_all(ids)
         records_table = {}
-        options.each do |k, v|
-          @model = model.send(k, v)
+        relation = options.reduce(model) do |rel, (k, v)|
+          rel.send(k, v)
         end
-        model.where(id: ids).each do |record|
+
+        relation.where(id: ids).each do |record|
           records_table[record.id] = record
         end
+
         ids.map do |id|
           records_table.select { |k, _| k == id.to_i }[id.to_i]
         end.reject(&:nil?)
